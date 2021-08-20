2b40fed5882e50f717a711cdda745e42df825d2f
Huang Meng <mark.huang1212@gmail.com>
2021-08-20 17:21:33 +0800

    add config comment

--- src/config.ts
+++ src/config.ts
@@ -12,9 +12,24 @@ import { env } from "process"
 
 class Config {
 
+    /**
+     * The host to which the client will connect
+     */
     host = env.HOST
+
+    /**
+     * Client/Server SSL Key
+     */
     key = '/etc/h2-proxy/key.pem'
+
+    /**
+     * Client/Server SSL Certificate
+     */
     cert = '/etc/h2-proxy/cert.pem'
+
+    /**
+     * CA For client authentication
+     */
     ca = '/etc/h2-proxy/ca.pem'
 
     server_port = 443
